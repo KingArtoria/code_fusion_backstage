6fccda4b95d2b4d0fed46e87e057c6b87db4a4ea
[AnTanZhi] <[gaojiuan@163.com]>
2022-04-18 15:35:43 +0800

    初始化项目

---
+++ src/utils/ajax.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+import { Message } from 'element-ui'
+const service = axios.create({
+  baseURL: 'https://asd.bdhuoke.com/api',
+  timeout: 5000
+})
+service.interceptors.response.use(
+  response => {
+    const res = response.data
+    if (res.code !== 200) {
+      Message({
+        message: res.msg,
+        type: 'error',
+        duration: 5 * 1000
+      })
+      return Promise.reject(new Error(res.msg || 'Error'))
+    } else {
+      return res
+    }
+  },
+  error => {
+    console.log('err' + error)
+    Message({
+      message: error.message,
+      type: 'error',
+      duration: 5 * 1000
+    })
+    return Promise.reject(error)
+  }
+)
+export default service
